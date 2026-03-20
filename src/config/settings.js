991144b19cb792e230ceaf28ee42d0c35af1cb64
Medocs Health Services <medocssl@gmail.com>
2026-03-20 18:10:32 +0530

    Update settings.js

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,8 +1,7 @@
 export default {
-	title: `Odyssey Astro Theme | A Marketing Website Theme for Startups and Businesses`,
-	description: `A simple, clean, and modern theme for a startup or businesses' marketing website.`,
-	url: `https://odyssey-theme.sapling.supply`, // No trailing slash!
-	name: `Odyssey`, // The short name of the business or brand name. Used for things like the copyright in the footer.
-	enableThemeSwitcher: true,
-	showPlug: true, // Disable this if you want to remove the plug from the footer. (╯°□°)╯︵ ┻━┻
+  title: 'MEDOCS Health Services (Pvt) Ltd',
+  description: 'Premium home-based medical care, ground ambulance services, and medical equipment rental.',
+  url: 'https://medocs.lk', // Update this to your actual domain later
+  name: 'MEDOCS',
+  showLittleSticksPlug: false, // Hides the theme developer's plug
 };
