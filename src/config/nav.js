6cdc0fec4402238897274072cbfab027ccc0d28c
Medocs Health Services <medocssl@gmail.com>
2026-03-20 18:21:08 +0530

    Update nav.js

--- src/config/nav.js
+++ src/config/nav.js
@@ -1,18 +1,6 @@
 export const nav = [
-  {
-    title: 'Home',
-    slug: '/',
-  },
-  {
-    title: 'Services',
-    slug: '/services',
-  },
-  {
-    title: 'About Us',
-    slug: '/company/about',
-  },
-  {
-    title: 'Contact',
-    slug: '/company/contact',
-  },
+  { title: 'Home', slug: '/' },
+  { title: 'Services', slug: '/services' },
+  { title: 'About Us', slug: '/company/about' },
+  { title: 'Contact', slug: '/company/contact' },
 ];
