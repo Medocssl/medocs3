2acfe458f572f9f66a2067786cbb62477d8564ba
Medocs Health Services <medocssl@gmail.com>
2026-03-21 00:56:41 +0530

    Update footer.js

--- src/config/footer.js
+++ src/config/footer.js
@@ -1,17 +1,32 @@
+export const footerSocials = [
+  {
+    name: 'Facebook',
+    url: 'https://facebook.com/medocs',
+    icon: "mdi:facebook",
+  },
+  {
+    name: 'Instagram',
+    url: 'https://instagram.com/medocs',
+    icon: "mdi:instagram",
+  },
+];
+
 export const footerLists = [
+  {
+    title: 'Services',
+    items: [
+      { title: 'Home Visits', slug: '/services' },
+      { title: 'Ambulance', slug: '/services' },
+      { title: 'Equipment Rental', slug: '/services' },
+      { title: 'Air Escorts', slug: '/services' },
+      { title: 'Palliative Care', slug: '/services' },
+    ],
+  },
   {
     title: 'Company',
     items: [
       { title: 'About Us', slug: '/company/about' },
       { title: 'Contact', slug: '/company/contact' },
-    ],
-  },
-  {
-    title: 'Services',
-    items: [
-      { title: 'Nurse Visits', slug: '/services' },
-      { title: 'Ambulance', slug: '/services' },
-      { title: 'Equipment', slug: '/services' },
     ],
   },
 ];
